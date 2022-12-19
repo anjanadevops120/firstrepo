c7e9f9b74b2f4be63e6e733111a237f020befed1
anjanadevops120 <121003160+anjanadevops120@users.noreply.github.com>
2022-12-19 10:42:57 +0530

    Update file1.cs

--- file1.cs
+++ file1.cs
@@ -4,7 +4,7 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World today!");
+            System.Console.WriteLine("Hello World monday !");
         }
     }
 }
